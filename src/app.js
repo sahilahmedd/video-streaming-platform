227a69acf1143d552a9e8d01a3d01cd23c652d8a
Sahil Ahmed <sahilahmedtcg@gmail.com>
2025-04-28 22:13:01 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -25,4 +25,7 @@ import userRouter from "./routes/user.routes.js";
 // routes Declaration
 // We don't directly use the app.get or app.post here because it is not the best practice isteade we use app.use() and define a standard route and then define the specifice routes in a saperate file
 app.use("/api/v1/users", userRouter)
+
+// http://localhost:3000/user/api/v1/register => register route
+
 export { app };
