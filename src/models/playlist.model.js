a1038bc581e16e4eaf12537e10db7d1e8fc97ecc
Sahil Ahmed <sahilahmedtcg@gmail.com>
2025-05-28 14:29:11 +0530

    Added comment, like, tweet, playlist models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Playlist = mongoose.modal("Playlist", playlistSchema);
