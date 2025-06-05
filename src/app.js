07b41e49323294e0c28e00ecffba38cf4c92cf16
Sahil Ahmed <sahilahmedtcg@gmail.com>
2025-06-05 14:34:07 +0530

    API router added

--- src/app.js
+++ src/app.js
@@ -22,18 +22,20 @@ app.use(cookieParser()); //To manage cookies CRUD opreations
 
 // routes Import
 import userRouter from "./routes/user.routes.js";
+import likeRouter from "./routes/likes.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import commentRouter from "./routes/comment.routes.js";
-import likeRouter from "./routes/likes.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import healthRouter from "./routes/healthcheck.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 
 // routes Declaration
 // We don't directly use the app.get or app.post here because it is not the best practice insteade we use app.use() and define a standard route and then define the specifice routes in a saperate file
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/users", tweetRouter);
-app.use("/api/v1/users", commentRouter);
 app.use("/api/v1/users", likeRouter);
+app.use("/api/v1/users", tweetRouter);
+app.use("/api/v1/users", healthRouter);
+app.use("/api/v1/users", commentRouter);
 app.use("/api/v1/users", playlistRouter);
 app.use("/api/v1/users", subscriptionRouter);
 
